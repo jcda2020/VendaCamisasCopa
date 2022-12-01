47626a8f7fda4c2f434bf08f0f120dd4d7801850
jcda2020 <jcdasistem@gmail.com>
2022-12-01 05:50:02 -0300

    add implents users

--- src/model/dao/UsersDaoImplents.java
+++ src/model/dao/UsersDaoImplents.java
@@ -1,11 +1,26 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import dao.UsersDao;
+import db.DB;
+import model.People;
 import model.Users;
+import model.People;
 
 public class UsersDaoImplents implements UsersDao{
+
+	private Connection conn;
+	
+	
+	public UsersDaoImplents(Connection conn) {
+		super();
+		this.conn = conn;
+	}
 
 	@Override
 	public void inseriUsers(Users user) {
@@ -21,7 +36,34 @@ public class UsersDaoImplents implements UsersDao{
 
 	@Override
 	public Users findById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT * FROM users WHERE id = ?";
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			
+			rs = stmt.executeQuery();
+			Users user = new Users(); 
+			People people = new People();
+			while (rs.next()) {			
+				//user.setCpf_people(people.setCpf(rs.getString("cpf_pessoas")));		
+			}
+			
+			return user;
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			DB.closeStatement(stmt);
+			DB.closeResultset(rs);
+		}
+		
+		
 		return null;
 	}
 
